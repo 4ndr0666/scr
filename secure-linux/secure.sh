5c51a7119a8e9f3211c34ed5ba1f6484617ffe92
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-13 22:46:23 -0500

    Update secure.sh
    updated

--- secure-linux/secure.sh
+++ secure-linux/secure.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
+sudo ufw --force reset;
+sudo ufw allow 22/tcp;
+sudo ufw allow 80,443/tcp;
+sudo ufw default deny incoming from any;
+sudo ufw default allow outgoing;
+sudo ufw enable;
 
-# Reset and enable firewall
-sudo ufw reload
-sudo ufw limit 22/tcp
-sudo ufw allow 80/tcp 443/tcp
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw enable
-
-# Harden /etc/sysctl.conf
+# --- Harden /etc/sysctl.conf
 sudo sysctl kernel.modules_disabled=1
-sudo sysctl -a --pattern 'net.ipv4.conf.all.rp_filter|net.ipv4.conf.all.arp'
+sudo sysctl -a
+sudo sysctl -A
+sudo sysctl mib
+sudo sysctl net.ipv4.conf.all.rp_filter
+sudo sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+
+# --- PREVENT IP SPOOFS
 cat <<EOF > /etc/host.conf
 order bind,hosts
 multi on
 EOF
 
-# Enable fail2ban
+# --- Enable fail2ban
 sudo cp fail2ban.local /etc/fail2ban/
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
-# Display listening ports
+echo "listening ports"
 sudo netstat -tunlp
+notify-send -i $notifyIcon "Firewall is up now";
 
-# Send notification
-notify-send -i $notifyIcon "Firewall is up now"
-
-exit 1
+exit 1;
