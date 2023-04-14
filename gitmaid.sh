1bb9c6bf6266428f1185ebe0c8db707956b9ea63
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-14 01:04:06 -0500

    Create gitmaid.sh
    t searches for all Git repositories under a specified directory, and reports whether each repository is clean or dirty. For dirty repositories, it also reports the status of any changed files. The script can take an optional argument specifying the search path, and can ignore specified directories. The output includes headings and colorized text to make it easier to read.

---
+++ gitmaid.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+VERSION="1.0"
+
+# Set the search path
+if [[ -z "$1" ]]; then
+    if [[ -z $SEARCHPATH ]]; then
+        SEARCHPATH=$PWD
+    fi
+else
+    if [[ $1 == '-p' ]]; then
+        SEARCHPATH=$PWD
+    else
+        SEARCHPATH="$1"
+    fi
+fi
+
+if [[ ! -d "$SEARCHPATH" ]]; then
+    echo "The supplied path does not resolve to a valid directory"
+    echo "Aborting..."
+    exit 1
+fi
+
+cd "$SEARCHPATH"
+
+# Find all directories that have a .git directory in them
+dirty_repos=0
+clean_repos=0
+for git_dir in $(find . -type d -name .git | sort | sed "s/\/\.git//"); do
+    # Save the current working directory before changing directories
+    pushd . >/dev/null
+    cd "$git_dir"
+
+    # Check if repository is dirty or clean
+    git_status=$(git status --porcelain)
+    if [[ -n "$git_status" ]]; then
+        dirty_repos=$((dirty_repos+1))
+        echo -e "${git_dir} \t \033[1;31mDirty\033[0m"
+    else
+        clean_repos=$((clean_repos+1))
+        echo -e "${git_dir} \t \033[1;32mClean\033[0m"
+    fi
+
+    # Return to the starting directory
+    popd >/dev/null
+done
+
+echo "Repositories checked:"
+echo "  - Dirty: ${dirty_repos}"
+echo "  - Clean: ${clean_repos}"
+
+if [[ $dirty_repos -gt 0 ]]; then
+    echo -e "\n\033[1;31mSome repositories are dirty. Please check the above output for details.\033[0m"
+else
+    echo -e "\n\033[1;32mAll repositories are clean!\033[0m"
+fi
