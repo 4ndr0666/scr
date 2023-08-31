496a85bc517b7769ca678000d5911e30ca6d909f
andro <97570105+4ndr0666@users.noreply.github.com>
2023-08-30 20:13:57 -0500

    added permission_reference.txt

--- deps.sh
+++ deps.sh
@@ -2,62 +2,51 @@
 
 # Create log directory if it doesn't exist
 log_dir="$HOME/.local/share/dependencies"
-mkdir -p $log_dir || { echo "Failed to create log directory"; exit 1; }
+mkdir -p "$log_dir" || exit_with_error "Failed to create log directory"
 
 # Log file to store installed dependencies
 log_file="$log_dir/dependency_log_$(date +%Y%m%d%H%M%S).log"
 
+# Unified error handling
+exit_with_error() {
+  echo "$1" | tee -a "$log_file"
+  exit 1
+}
+
 # Function to display a progressively expanding progress bar in cyan
 display_progress_bar() {
-  echo -ne "\033[36mProcessing: "
-  for i in $(seq 1 50); do # Adjust 50 to the desired length of the progress bar
-    echo -ne "="
+  printf "\033[36mProcessing: "
+  for i in $(seq 1 50); do
+    printf "="
     sleep 0.1
   done
-  echo -e "\033[38;5;114m Done\033[0m! ✨ 🌟 ✨"
+  printf "\033[38;5;114m Done\033[0m! ✨ 🌟 ✨\n"
 }
 
 # Function to check if a package is installed
 is_installed() {
-  paru -Q $1 &> /dev/null || { echo "Error: Failed to query package $1"; return 1; }
+  local pkg="$1"
+  paru -Q "$pkg" &> /dev/null || exit_with_error "Error: Failed to query package $pkg"
 }
 
 # Function to get the repository of a package
 get_repo() {
-  repo=$(paru -Si $1 | grep "Repository" | awk '{print $3}') || { echo "Error: Failed to get repository for $1"; return 1; }
-  echo $repo
+  local pkg="$1"
+  paru -Si "$pkg" | grep "Repository" | awk '{print $3}' || exit_with_error "Error: Failed to get repository for $pkg"
 }
 
 # Function to recursively find and install missing dependencies using pactree
 install_missing_deps() {
-  local pkg=$1
-  deps=$(pactree -l $pkg) || { echo "Error: Failed to get dependencies for $pkg"; return 1; }
+  local pkg="$1"
+  local deps=$(pactree -l "$pkg") || exit_with_error "Error: Failed to get dependencies for $pkg"
+  
   for dep in $deps; do
-    if ! is_installed $dep; then
-      echo "Missing dependency found: $dep"
-      read -p "Do you want to install it? (y/n): " choice
-      if [ "$choice" == "y" ]; then
-        repo=$(get_repo $dep)
-        paru -S $dep || { echo "Error: Failed to install $dep"; return 1; }
-        echo "Successfully installed $dep from $repo"
-        echo "$repo: $dep installed on $(date)" >> $log_file
-        install_missing_deps $dep
-      else
-        echo "$dep skipped"
-        echo "$dep skipped" >> $log_file
-      fi
-    fi
-  done
-}
-
-# Function to rollback installed packages
-rollback() {
-  tac $log_file | while read -r line; do
-    pkg=$(echo $line | awk '{print $2}')
-    read -p "Do you want to uninstall $pkg? (y/n): " choice
-    if [ "$choice" == "y" ]; then
-      paru -Rns $pkg || { echo "Error: Failed to uninstall $pkg"; return 1; }
-      echo "Successfully uninstalled $pkg"
+    if ! is_installed "$dep"; then
+      echo "Missing dependency found: $dep" | tee -a "$log_file"
+      local repo=$(get_repo "$dep")
+      paru -S "$dep" || exit_with_error "Error: Failed to install $dep"
+      echo "Successfully installed $dep from $repo" | tee -a "$log_file"
+      install_missing_deps "$dep"
     fi
   done
 }
@@ -65,17 +54,16 @@ rollback() {
 # Main function
 main() {
   display_progress_bar
-  if [ "$1" == "rollback" ]; then
+  local pkg_name="$1"
+  
+  if [ "$pkg_name" == "rollback" ]; then
     rollback
+  elif ! is_installed "$pkg_name"; then
+    exit_with_error "The package $pkg_name is not installed. Exiting."
   else
-    pkg_name=$1
-    if ! is_installed $pkg_name; then
-      echo "The package $pkg_name is not installed. Exiting."
-      exit 1
-    fi
-    install_missing_deps $pkg_name
+    install_missing_deps "$pkg_name"
     echo "Process complete. Check $log_file for a log of changes."
   fi
 }
 
-main $1
+main "$1"
