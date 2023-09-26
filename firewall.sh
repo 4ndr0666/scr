ad8c05218893c30e8707fe4caa48d8ae58afd4ef
andro <97570105+4ndr0666@users.noreply.github.com>
2023-09-26 17:45:41 -0500

    final firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,61 +1,136 @@
 #!/bin/bash
 
-# Automatically escalate privileges if not running as root
+# -- Advanced UFW Rules (Comments):
+# sudo ufw limit proto tcp from any to any port 22
+# sudo ufw allow from 192.168.0.0/24
+# sudo ufw deny from 192.168.1.0/24
+# sudo ufw allow ssh
+# sudo ufw allow in on eth0 to any port 80
+# sudo ufw logging on
+
+# --- Automatically escalate privileges if not running as root
 if [ "$(id -u)" -ne 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
 
+# --- Function to handle errors
+handle_error() {
+  if [ $? -ne 0 ]; then
+    echo "Error: $1"
+    exit 1
+  fi
+}
+
+# --- Check and set permissions for /var/log
+chmod 755 "/var/log"
+chown root:root "/var/log"
+echo "Permissions and ownership set for /var/log"
+handle_error "Failed to set permissions for /var/log"
+
+# --- Cleanup: Keep only the most recent backup and remove older ones
+cleanup_backups() {
+    cd /etc/ufw
+    for rule_file in before.rules before6.rules after.rules after6.rules user.rules user6.rules; do
+        # Sort the files by modification time in reverse order and skip the first one (most recent backup)
+        ls -t ${rule_file}.* 2>/dev/null | tail -n +2 | xargs -r -I {} rm -- {}
+    done
+    cd -
+}
+
 # --- Check status
 sysctl -a | grep disable_ipv6
 
 # --- Harden /etc/sysctl.conf
-sudo sysctl kernel.modules_disabled=1
-sudo sysctl net.ipv4.conf.all.rp_filter=1
-sudo sysctl -a
-sudo sysctl -A
-sudo sysctl mib
+sysctl kernel.modules_disabled=1
+sysctl net.ipv4.conf.all.rp_filter=1
+sysctl -a
+sysctl -A
+# sudo sysctl mib  # Commented out due to error
 
-# --- Disable IPv6 globally:
-IPv6_SETTINGS=("net.ipv6.conf.all.disable_ipv6 = 1" "net.ipv6.conf.default.disable_ipv6 = 1" "net.ipv6.conf.lo.disable_ipv6 = 1" "net.ipv6.conf.tun0.disable_ipv6 = 1")
-for setting in "${IPv6_SETTINGS[@]}"; do
-    sudo bash -c "echo $setting >> /etc/sysctl.conf"
-done
-sudo sysctl -p
-
-# 1. Ensure only ExpressVPN is running
+# --- 1. Ensure only ExpressVPN is running
 if systemctl list-units --type=service | grep -q "openvpn.service"; then
-    sudo systemctl stop openvpn
-    sudo systemctl disable openvpn
+    systemctl stop openvpn
+    systemctl disable openvpn
 fi
 
-# 2. If not using dynamic IP addressing, stop and disable dhcpcd
+# --- 2. If not using dynamic IP addressing, stop and disable dhcpcd
 if systemctl list-units --type=service | grep -q "dhcpcd.service"; then
-    sudo systemctl stop dhcpcd
-    sudo systemctl disable dhcpcd
+    systemctl stop dhcpcd
+    systemctl disable dhcpcd
 fi
 
-# 3. Restrict SSH to localhost if only needed for Git
-# For SSH to listen only on localhost (IPv4):
-sudo sed -i 's/^#ListenAddress 0.0.0.0/ListenAddress 127.0.0.1/' /etc/ssh/sshd_config
-# For SSH to listen only on localhost (IPv6):
-sudo sed -i 's/^#ListenAddress ::/ListenAddress ::1/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# --- 3. Restrict SSH to localhost if only needed for Git
+sed -i 's/^#ListenAddress 0.0.0.0/ListenAddress 127.0.0.1/' /etc/ssh/sshd_config
+sed -i 's/^#ListenAddress ::/ListenAddress ::1/' /etc/ssh/sshd_config
+systemctl restart sshd
 
-# 4 & 5. Kill all IPv6 for SSH and other services
-sudo sed -i 's/^#AddressFamily any/AddressFamily inet/' /etc/ssh/sshd_config
-# IPv6 is now managed via UFW and service configurations, no need to disable it globally.
+# --- 4. UFW Setup (with megasync rule)
+# Check and set permissions for UFW directory
+UFW_DIR="/etc/ufw/"
 
-# UFW Setup
-# IPv4 rules:
-sudo ufw limit 22/tcp    # Limit SSH connections over IPv4 to prevent brute-force attacks
-# Corresponding IPv6 rules (assuming you want similar behaviors for IPv6):
-sudo ufw limit from any to any port 22 proto ipv6   # Limit SSH connections over IPv6 to prevent brute-force attacks
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw default deny incoming v6
-sudo ufw default deny outgoing v6
-sudo ufw enable
+if [ -d "$UFW_DIR" ]; then
+    chmod 700 "$UFW_DIR"  # Changed to 700 for directory and files
+    echo "Permissions set for $UFW_DIR"
+else
+    echo "Directory $UFW_DIR does not exist"
+fi
+
+# Check and set permissions for UFW rules files
+UFW_FILES=("/etc/ufw/user.rules" "/etc/ufw/before.rules" "/etc/ufw/after.rules" 
+           "/etc/ufw/user6.rules" "/etc/ufw/before6.rules" "/etc/ufw/after6.rules")
+
+for file in "${UFW_FILES[@]}"; do
+    if [ -f "$file" ]; then
+        chmod 600 "$file"
+        echo "Permissions set for $file"
+    else
+        echo "File $file does not exist"
+    fi
+done
+
+# --- Disable UFW and reset all rules
+ufw --force reset
+
+# --- IPv6-related UFW settings, skip entirely if IPv6 is disabled
+# 3.1 Robust check for IPv6
+if [ $(cat /proc/sys/net/ipv6/conf/all/disable_ipv6) -eq 1 ]; then
+    echo "IPv6 is disabled. Skipping IPv6 setup."
+else
+    ufw limit from any to any port 22 proto ipv6
+    ufw default deny incoming v6
+    ufw default deny outgoing v6
+fi
+handle_error "Failed to configure IPv6 in UFW" 
+
+# --- Enter rules
+ufw limit 22/tcp
+ufw limit from any to any port 22 proto ipv6
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 6341/tcp  # Allow megasync
+ufw default deny incoming v6
+ufw default deny outgoing v6
+ufw enable
+
+# --- Check for existence of /etc/ufw/user.rules
+# Declare an array of UFW rule files to check
+UFW_FILES=("/etc/ufw/user.rules" "/etc/ufw/before.rules" "/etc/ufw/after.rules" 
+           "/etc/ufw/user6.rules" "/etc/ufw/before6.rules" "/etc/ufw/after6.rules")
+
+# Loop through each file to create a backup
+for file in "${UFW_FILES[@]}"; do
+    if [ -f "$file" ]; then
+        backup_file="${file}.bak"  # Append '.bak' for backup
+        cp "$file" "$backup_file"
+        echo "Backup created: $backup_file"
+    else
+        echo "File $file does not exist. Skipping backup."
+    fi
+done
+
+# Invoke the cleanup function here (assumes cleanup_backups is defined elsewhere in your script)
+cleanup_backups  
 
 # --- PREVENT IP SPOOFS
 cat <<EOF | sudo tee /etc/host.conf
@@ -64,75 +139,48 @@ multi on
 EOF
 
 # --- Enable fail2ban
-if [ -f "/usr/local/bin/jail.local" ]; then
-    sudo cp /usr/local/bin/jail.local /etc/fail2ban/
-fi
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
+if [ -x "$(command -v fail2ban-client)" ]; then
+    # 4.2 Write jail.local directly to /etc/fail2ban/
+    cat <<EOF > /etc/fail2ban/jail.local
+    [DEFAULT]
+    ignoreip = 127.0.0.1/8
+    findtime = 600
+    bantime = 3600
+    maxretry = 5
 
-# Replace and backup jail.local
-if [ -f "/etc/fail2ban/jail.local" ]; then
-    sudo cp /etc/fail2ban/jail.local /etc/fail2ban/jail.local.bak
-fi
-cat <<EOF | sudo tee /etc/fail2ban/jail.local
-[DEFAULT]
-ignoreip = 127.0.0.1/8
-findtime = 600
-bantime = 3600
-maxretry = 5
+    [sshd]
+    enabled = true
+    port = ssh
+    filter = sshd
+    logpath = /var/log/auth.log
+    maxretry = 3
 
-[sshd]
-enabled = true
-port = ssh
-filter = sshd
-logpath = /var/log/auth.log
-maxretry = 3
-[ufw]
-enabled = true
-port = all
-filter = ufw
-logpath = /var/log/ufw.log
-maxretry = 5
-[openvpn]
-enabled = false
-port = 1194
-protocol = udp
-filter = openvpn
-logpath = /var/log/openvpn.log
-maxretry = 3
-backend = auto
-usedns = warn
-logencoding = auto
-enabled = false
-filter = %(__name__)s[mode=%(mode)s]
-destemail = root@localhost
-sender = root@localhost
-protocol = tcp
-chain = INPUT
-port = 0:65535
-fail2ban_agent = Fail2Ban/%(fail2ban_version)s
-banaction = iptables-multiport
-banaction_allports = iptables-allports
-action_ = %(banaction)s[name=%(__name__)s, bantime="%(bantime)s", port="%(port)s", protocol="%(protocol)s", chain="%(chain)s"]
-action = %(action_)s
+    [ufw]
+    enabled = true
+    port = all
+    filter = ufw
+    logpath = /var/log/ufw.log
+    maxretry = 5
 EOF
+    handle_error "Failed to configure Fail2Ban"  # 1.1
 
-# Restart Fail2Ban to apply new policies
+    systemctl enable fail2ban
+    systemctl restart fail2ban
+else
+    echo "Fail2Ban is not installed. Skipping Fail2Ban configuration."
+fi
+
+# --- Restart Fail2Ban to apply new policies
+sudo systemctl enable fail2ban
 sudo systemctl restart fail2ban
 
-# Human-readable summary of the portscan
+# --- Human-readable summary of the portscan
 echo "-----------------------------------"
 echo "HUMAN-READABLE SUMMARY OF PORTSCAN:"
 echo "-----------------------------------"
 sudo netstat -tunlp | grep LISTEN | awk '{print $1, $4, $7}' | sed 's/:::/IPv6 /' | sed 's/0.0.0.0:/IPv4 /'
 
-# Exit with success status
+# --- Exit with success status
 exit 0
 
-# Advanced UFW Rules (Comments):
-# sudo ufw limit proto tcp from any to any port 22
-# sudo ufw allow from 192.168.0.0/24
-# sudo ufw deny from 192.168.1.0/24
-# sudo ufw allow ssh
-# sudo ufw allow in on eth0 to any port 80
-# sudo ufw logging on
+
