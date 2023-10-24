72b6bf948f982e45c1c1c14fb04bc9398b190ce4
andro <97570105+4ndr0666@users.noreply.github.com>
2023-10-24 01:02:27 -0500

    upgrade quick-secure and added vapoursynth script

--- quick-secure.sh
+++ quick-secure.sh
@@ -1,50 +1,159 @@
 #!/bin/bash
 
-# Check if script is running with root permissions
-if [[ $UID != 0 ]]; then
-  echo "Sorry, must sudo or be root to run this."
+# === // QUICK-SECURE.SH // ======== ||
+GREEN='\033[0;32m'
+BOLD='\033[1m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Symbols for visual feedback
+SUCCESS="✔️"
+FAILURE="❌"
+INFO="➡️"
+EXPLOSION="💥"
+
+# Function to display prominent messages
+prominent() {
+    echo -e "${BOLD}${GREEN}$1${NC}"
+}
+
+# Function for errors
+bug() {
+    echo -e "${BOLD}${RED}$1${NC}"
+}
+
+# Logging function
+log() {
+    echo "$(date): $1" >> /var/log/r8169_module_script.log
+}
+
+# Print ASCII art in green
+echo -e "${GREEN}"
+cat << "EOF"
+#  ________        .__        __                                                                   .__
+#  \_____  \  __ __|__| ____ |  | __           ______ ____   ____  __ _________   ____        _____|  |__
+#   /  / \  \|  |  \  |/ ___\|  |/ /  ______  /  ___// __ \_/ ___\|  |  \_  __ \_/ __ \      /  ___/  |  \
+#  /   \_/.  \  |  /  \  \___|    <  /_____/  \___ \\  ___/\  \___|  |  /|  | \/\  ___/      \___ \|   Y  \
+#  \_____\ \_/____/|__|\___  >__|_ \         /____  >\___  >\___  >____/ |__|    \___  > /\ /____  >___|  /
+#         \__>             \/     \/              \/     \/     \/                   \/  \/      \/     \/
+EOF
+echo -e "${NC}"
+
+# --- Check for root:
+if [[ $UID != "0" ]]; then
+  prominent "$INFO Escalate your privileges."
+  exit
+fi
+
+# --- Display disabled lines as options:
+if [[ $1 = "-c" ]]; then
+  echo ""
+  prominent "$SUCCESS === // ENANBLED ACTIONS // === $SUCCESS"
+  cat $0 | grep ^[A-Z]
+  echo ""
+  prominent "$FAILURE === // DISABLED ACTIONS // === $FAILURE"
+  grep ^#[Aa-Zz] $0
+  exit
+fi
+
+if [[ $1 = "-u" ]]; then
+  echo ""
+  cat $0 | sed 's/^[ \t]*//;s/[ \t]*$//' | grep '.' | grep -v ^#
+  bug "Something broke"
+  log "Something broke"
   exit 1
 fi
 
-# Set variables of script
+# --- Env variables:
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/local/bin:/opt/local/sbin"
+PASS_EXP="60" #used to set password expire in days
+PASS_WARN="14" #used to set password warning in days
+PASS_CHANG="1" #used to set how often you can change password in days
+SELINUX=`grep ^SELINUX= /etc/selinux/config 2>/dev/null | awk -F'=' '{ print $2 }'
 
-# Verify admin wants to harden system
-read -p "Are you sure you want to quick secure `hostname` (y/N)? " ANSWER
+# --- Menu:
+prominent "$INFO"
+cat << 'DISC'
+# === // CHOOSE INSTALLATION METHOD //
 
-if [[ $ANSWER != "y" ]]; then
+  quick-secure -c | Review what's commented out in script
+  quick-secure -u | Review what's being applied to system
+  quick-secure -f | Force settings, never prompt question
+DISC
+
+# --- Confirmation:
+if [[ $1 != "-f" ]]; then
+  prominent "$INFO -n Begin system scan on `hostname` (y/N)? "
+  read ANSWER
+  if [[ $ANSWER != "y" ]]; then
     echo ""
-    exit 1
+    exit
+  fi
+
+  if [[ $ANSWER = "n" ]] || [[ $ANSWER = "" ]]; then
+    echo ""
+    exit
+  fi
+
+  prominent ""
 fi
 
-# Set audit group variable
-AUDIT=$(getent group audit | cut -d: -f1)
-AUDIT=${AUDIT:-root}
+# --- Set audit group variable:
+if [[ `grep -i ^audit /etc/group` != "" ]]; then
+  AUDIT=`grep -i ^audit /etc/group | awk -F":" '{ print $1 }' | head -n 1`
+else
+  AUDIT="root"
+fi
 
-echo "Audit group is set to '$AUDIT'"
-echo ""
+prominent "$INFO Audit group is set to '$AUDIT' $EXPLOSION"
+prominent ""
 
-# Setup /etc/motd and /etc/issues
-echo "I've read & consent to terms in IS user agreem't." > /etc/motd # STIG V-38593
-echo "I've read & consent to terms in IS user agreem't." > /etc/issue* # STIG V-38593
 
-chown root:root /etc/motd /etc/issue*
-chmod 0444 /etc/motd /etc/issue*
+# --- Temp disable selinux:
+if [[ `getenforce 2>/dev/null` = "Enforcing" ]]; then
+  setenforce 0
+fi
 
-# Cron setup (simplified)
-touch /etc/cron.allow /etc/at.allow
-echo "root" >| /etc/cron.allow
-echo "root" >| /etc/at.allow
+if [[ -f /etc/sysconfig/selinux ]]; then
+  sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+  echo "SELINUX=disabled" > /etc/sysconfig/selinux
+  echo "SELINUXTYPE=targeted" >> /etc/sysconfig/selinux
+  chmod -f 0640 /etc/sysconfig/selinux
+fi
 
-rm -f /etc/at.deny
-rm -f /etc/cron.deny
+# --- Cron Job -------- ||
+if [[ -f /etc/cron.allow ]]; then
+  if [[ `grep root /etc/cron.allow 2>/dev/null` != "root" ]]; then
+    echo "root" > /etc/cron.allow
+    rm -f /etc/at.deny
+  else
+    prominent "$FAILURE qroot is already in /etc/cron.allow"
+    prominent ""
+  fi
+fi
 
-chmod 0400 /etc/cron.allow
-chmod 0400 /etc/at.allow
+if [[ -f /etc/cron.allow ]]; then
+  if [[ ! -f /etc/at.allow ]]; then
+    touch /etc/at.allow
+  fi
+fi
 
-# File permissions and ownerships (simplified)
-chown root:root /var/crash
-chmod 0600 /var/crash
+if [[ `grep root /etc/at.allow 2>/dev/null` != "root" ]]; then
+  echo "root" > /etc/at.allow
+  rm -f /etc/at.deny
+else
+  prominent "$FAILURE root is already in /etc/at.allow"
+  echo ""
+fi
+
+if [[ `cat /etc/at.deny 2>/dev/null` = "" ]]; then
+  rm -f /etc/at.deny
+fi
+
+if [[ `cat /etc/cron.deny 2>/dev/null` = "" ]]; then
+  rm -f /etc/cron.deny
+fi
+
 chmod -f 0700 /etc/cron.monthly/*
 chmod -f 0700 /etc/cron.weekly/*
 chmod -f 0700 /etc/cron.daily/*
@@ -66,7 +175,7 @@ chmod -f 0600 /var/spool/at/*
 chmod -f 0400 /etc/anacrontab
 
 
-#File permissions and ownerships
+# === // Perms and Ownership // ======== ||
 chmod -f 1777 /tmp
 chown -f root:root /var/crash
 chown -f root:root /var/cache/mod_proxy
@@ -211,7 +320,7 @@ chmod -f 0750 /etc/chatscripts
 chmod -f 0750 /usr/local/share/ca-certificates
 
 
-#ClamAV permissions and ownership
+# --- ClamAV permissions and ownership:
 if [[ -d /usr/local/share/clamav ]]; then
   passwd -l clamav 2>/dev/null
   usermod -s /sbin/nologin clamav 2>/dev/null
@@ -236,7 +345,7 @@ if [[ -d /var/clamav ]]; then
 fi
 
 
-#DISA STIG file ownsership
+# --- DISA STIG file ownsership:
 chmod -f 0755 /bin/csh
 chmod -f 0755 /bin/jsh
 chmod -f 0755 /bin/ksh
@@ -292,76 +401,30 @@ chmod -f 0755 /usr/bin/sh
 chmod -f 1777 /var/mail
 chmod -f 1777 /var/spool/uucppublic
 
-#Set all files in ``.ssh`` to ``600``
+# --- Set all files in ``.ssh`` to ``600``:
 chmod 700 ~/.ssh && chmod 600 ~/.ssh/*
 
-# Remove security related packages
+# --- Remove security related packages:
 if [[ -f /bin/yay ]]; then
-  for pkg in nc vsftpd telnet-server rdate tcpdump vnc-server tigervnc-server wireshark wireless-tools telnetd rdate vnc4server vino bind9-host libbind9-90; do
+  for pkg in vsftpd telnet-server rdate tcpdump vnc-server tigervnc-server wireshark wireless-tools telnetd rdate vnc4server vino bind9-host libbind9-90; do
     yay -Rdd $pkg 2>/dev/null
   done
 fi
 
-# Account management and cleanup
+# --- Account management and cleanup:
 if [[ $(which userdel 2>/dev/null) != "" ]]; then
   for user in games news gopher tcpdump shutdown halt sync ftp operator lp uucp irc gnats pcap netdump; do
     userdel -f $user 2>/dev/null
   done
 fi
 
-# Set basic kernel parameters
-if [[ $(which sysctl 2>/dev/null) != "" ]]; then
-  # Turn on ASLR Conservative Randomization
-  sysctl -w kernel.randomize_va_space=1
-
-#Set basic kernel parameters
-if [[ `which sysctl 2>/dev/null` != "" ]]; then
-#  #Turn on Exec Shield for RHEL systems
-#  sysctl -w kernel.exec-shield=1
-  #Turn on ASLR Conservative Randomization
-  sysctl -w kernel.randomize_va_space=1
-  #Hide Kernel Pointers
-  sysctl -w kernel.kptr_restrict=1
-  #Allow reboot/poweroff, remount read-only, sync command
-  sysctl -w kernel.sysrq=176
-  #Restrict PTRACE for debugging
-  sysctl -w kernel.yama.ptrace_scope=1
-  #Hard and Soft Link Protection
-  sysctl -w fs.protected_hardlinks=1
-  sysctl -w fs.protected_symlinks=1
-  #Enable TCP SYN Cookie Protection
-  sysctl -w net.ipv4.tcp_syncookies=1
-  #Disable IP Source Routing
-  sysctl -w net.ipv4.conf.all.accept_source_route=0
-  #Disable ICMP Redirect Acceptance
-  sysctl -w net.ipv4.conf.all.accept_redirects=0
-  sysctl -w net.ipv6.conf.all.accept_redirects=0
-  sysctl -w net.ipv4.conf.all.send_redirects=0
-  sysctl -w net.ipv6.conf.all.send_redirects=0
-  #Enable IP Spoofing Protection
-  sysctl -w net.ipv4.conf.all.rp_filter=1
-  sysctl -w net.ipv4.conf.default.rp_filter=1
-  #Enable Ignoring to ICMP Requests
-  sysctl -w net.ipv4.icmp_echo_ignore_all=1
-  #Enable Ignoring Broadcasts Request
-  sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
-  #Enable Bad Error Message Protection
-  sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
-  #Enable Logging of Spoofed Packets, Source Routed Packets, Redirect Packets
-  sysctl -w net.ipv4.conf.all.log_martians=1
-  sysctl -w net.ipv4.conf.default.log_martians=1
-#  #Perfer Privacy Addresses
-#  sysctl -w net.ipv6.conf.all.use_tempaddr = 2
-#  sysctl -w net.ipv6.conf.default.use_tempaddr = 2
-#  sysctl -p
-fi
-
-#Disable fingerprint in PAM and authconfig
+# --- Disable fingerprint in PAM and authconfig:
 if [[ `which authconfig 2>/dev/null` != "" ]]; then
   authconfig --disablefingerprint --update
 fi
 
-#Start-up chkconfig levels set
+
+# --- Start-up chkconfig levels set:
 if [[ -f /sbin/chkconfig ]]; then
   /sbin/chkconfig --level 12345 auditd on 2>/dev/null
   /sbin/chkconfig isdn off 2>/dev/null
@@ -369,11 +432,49 @@ if [[ -f /sbin/chkconfig ]]; then
   /sbin/chkconfig haldaemon off 2>/dev/null #NEEDED ON FOR RHEL6 GUI
 fi
 
-#Misc settings and permissions
+# --- // Enable mount points nodev, noexec and nosuid by default: // -------- ||
+/boot
+sed -i "s/\( \/boot.*`grep " \/boot " /etc/fstab | awk '{print $4}'`\)/\1,nodev,n
+oexec,nosuid/" /etc/fstab
+
+/dev/shm
+sed -i "s/\( \/dev\/shm.*`grep " \/dev\/shm " /etc/fstab | awk '{print $4}'`\)/\1
+,nodev,noexec,nosuid/" /etc/fstab
+
+/var
+sed -i "s/\( \/var\/log.*`grep " \/var " /etc/fstab | awk '{print $4}'`\)/\1,node
+v,noexec,nosuid/" /etc/fstab
+
+/var/log
+sed -i "s/\( \/var\/log.*`grep " \/var\/log " /etc/fstab | awk '{print $4}'`\)/\1
+,nodev,noexec,nosuid/" /etc/fstab
+
+/tmp
+sed -i "s/\( \/tmp.*`grep " \/tmp " /etc/fstab | awk '{print $4}'`\)/\1,nodev,noe
+xec,nosuid/" /etc/fstab
+
+/home
+sed -i "s/\( \/home.*`grep " \/home " /etc/fstab | awk '{print $4}'`\)/\1,nodev,n
+osuid/" /etc/fstab
+
+# --- Misc settings and permissions:
 chmod -Rf o-w /usr/local/src/*
 rm -f /etc/security/console.perms
 
-# Permit ssh login from root
+# ---Remove .pacnew files:
+nohup pacdiff > /dev/null 2>&1 &
+
+# --- Set background image permissions:
+if [[ -d /usr/share/backgrounds ]]; then
+  chmod -f 0444 /usr/share/backgrounds/default*
+  chmod -f 0444 /usr/share/backgrounds/images/default*
+fi
+
+if [[ $SELINUX = enforcing || $SELINUX = permissive ]]; then
+  setenforce 1
+fi
+
+# --- Permit ssh login from root:
 rootLogin='PermitRootLogin'
 sshConfig='/etc/ssh/sshd_config'
 
@@ -383,14 +484,61 @@ if [[ -f ${sshConfig?} ]]; then
   else
     echo 'PermitRootLogin no' >> ${sshConfig?}
   fi
-
-systemctl restart sshd.service
+  systemctl restart sshd.service
 fi
 
-# Set home directories to 0700 permissions (simplified)
-for dir in /home/* /export/home/*; do
-   [ -d "$dir" ] && chmod 0700 "$dir"
-done
+# --- Chmod 700 home dirs:
+if [[ -d /home ]]; then
+  for x in `find /home -maxdepth 1 -mindepth 1 -type d`; do chmod -f 0700 $x; done
+fi
 
-echo "Starting pacdigg to handle .pacnew and .pacsave files..."
-nohup pacdiff > /dev/null 2>&1 &
+if [[ -d /export/home ]]; then
+  for x in `find /export/home -maxdepth 1 -mindepth 1 -type d`; do chmod -f 0700 $
+x; done
+fi
+
+# --- Kernel parameters:
+if [[ `which sysctl 2>/dev/null` != "" ]]; then
+  # --- Turn on ASLR Conservative Randomization:
+  sysctl -w kernel.randomize_va_space=1
+  # --- Hide Kernel Pointers:
+  sysctl -w kernel.kptr_restrict=1
+  # --- Allow reboot/poweroff, remount read-only, sync command:
+  sysctl -w kernel.sysrq=176
+  # --- Restrict PTRACE for debugging:
+  sysctl -w kernel.yama.ptrace_scope=1
+  # --- Hard and Soft Link Protection:
+  sysctl -w fs.protected_hardlinks=1
+  sysctl -w fs.protected_symlinks=1
+  # --- Enable TCP SYN Cookie Protection:
+  sysctl -w net.ipv4.tcp_syncookies=1
+  # --- Disable IP Source Routing:
+  sysctl -w net.ipv4.conf.all.accept_source_route=0
+  # --- Disable ICMP Redirect Acceptance:
+  sysctl -w net.ipv4.conf.all.accept_redirects=0
+  sysctl -w net.ipv6.conf.all.accept_redirects=0
+  sysctl -w net.ipv4.conf.all.send_redirects=0
+  sysctl -w net.ipv6.conf.all.send_redirects=0
+  # --- Enable IP Spoofing Protection:
+  sysctl -w net.ipv4.conf.all.rp_filter=1
+  sysctl -w net.ipv4.conf.default.rp_filter=1
+  # --- Enable Ignoring to ICMP Requests:
+  sysctl -w net.ipv4.icmp_echo_ignore_all=1
+  # --- Enable Ignoring Broadcasts Request:
+  sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+  # --- Enable Bad Error Message Protection:
+  sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+  # --- Enable Logging of Spoofed Packets, Source Routed Packets, Redirect Packets:
+  sysctl -w net.ipv4.conf.all.log_martians=1
+  sysctl -w net.ipv4.conf.default.log_martians=1
+  # --- Perfer Privacy Addresses:
+  sysctl -w net.ipv6.conf.all.use_tempaddr = 2
+  sysctl -w net.ipv6.conf.default.use_tempaddr = 2
+  sysctl -p
+fi
+
+prominent "$SUCCESS SCAN COMPLETED... $EXPLOSION"
+
+prominent "$EXPLOSION SYSTEM QUICKLY SECURED $EXPLOSION"
+
+exit 0
