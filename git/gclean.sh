53e831e01afc20ca2c28a881d644a9731a7c029d
andro <97570105+4ndr0666@users.noreply.github.com>
2023-12-21 19:38:35 -0600

    updated perms.sh and made gclean secure with diff sshkey

---
+++ git/gclean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# --- // Securely_add_ssh_key:
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+# --- // Start_here:
+cd /home/Build/git/clone
+
+# --- // Command_loop:
+for d in */ ; do
+    echo "Processing directory $d"
+    cd "$d"
+    gh tidy
+    cd ..
+done
