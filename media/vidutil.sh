4cd9953838027da62b4413df24ec5b3246df55c3
andro <97570105+4ndr0666@users.noreply.github.com>
2023-12-27 22:59:23 -0600

    updated vidutil.sh

--- media/vidutil.sh
+++ media/vidutil.sh
@@ -1,123 +1,165 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh
+# --- // 4ndr0666_Video_Utility_Script // ========
 
-# Color Codes
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-RESET='\033[0m'
-
-# Print banner
-echo -e "${GREEN}"
+# --- // BANNER:
+echo -e "\033[34m"
 cat << "EOF"
-  [Your ASCII Art Banner Here]
+  ____   ____.__    .___      __  .__.__              .__
+  \   \ /   /|__| __| _/_ ___/  |_|__|  |        _____|  |__
+   \   Y   / |  |/ __ |  |  \   __\  |  |       /  ___/  |  \
+    \     /  |  / /_/ |  |  /|  | |  |  |__     \___ \|   Y  \
+     \___/   |__\____ |____/ |__| |__|____/ /\ /____  >___|  /
+                     \/                     \/      \/     \/
 EOF
-echo -e "${RESET}"
+echo -e "\033[0m"
 
-# Error Handling Function
-error_exit() {
-    echo -e "${RED}ERROR: $1${RESET}" >&2
-    exit ${2-1}
-}
-
-# Check if FFmpeg is installed
-if ! command -v ffmpeg &> /dev/null; then
-    error_exit "FFmpeg could not be found. Please install it and try again."
-fi
-
-# Handle command-line arguments
-if [[ $# -lt 1 ]]; then
-    error_exit "Usage: $0 <video_file> [additional_options]"
-fi
-
-INPUT_VIDEO="$1"
-shift # Remove the first argument and shift the rest to the left
-
-# Check if the video file exists
-if [[ ! -f "$INPUT_VIDEO" ]]; then
-    error_exit "The specified video file does not exist: $INPUT_VIDEO"
-fi
-
-# Global variables
-OUTPUT_VIDEO="output" # Default output video name
-DATA_DIR="${PWD}/frame_captures"
-mkdir -p "$DATA_DIR"
-
-# Function to capture frames
+# Function to capture frames from a video file
 capture_frames() {
-    echo "Capturing frames from $INPUT_VIDEO"
-    local fps=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "$INPUT_VIDEO" | bc -l)
-    ffmpeg -i "$INPUT_VIDEO" -vf "fps=$fps" -qscale:v 2 -strftime 1 "$DATA_DIR/out-%Y%m%d%H%M%S.png"
+    local video=$1
+    local fps=${2:-30}  # Default FPS is 30 if not provided
+    local data_dir="frame_captures"
+
+    if [[ ! -f "$video" ]]; then
+        echo "Error: The video file does not exist."
+        return 1
+    fi
+
+    mkdir -p "$data_dir"
+
+    if [[ ! $video =~ \.(mp4|avi|mkv)$ ]]; then
+        echo "Error: Unsupported video format. Supported formats are MP4, AVI, and MKV."
+        return 1
+    fi
+
+    ffmpeg -i "$video" -vf "fps=$fps" "${data_dir}/frame_%04d.png" 2>&1 | tee capture_frames.log
+
+    if [ "${PIPESTATUS[0]}" -ne 0 ]; then
+        echo "Error: Failed to capture frames. Check capture_frames.log for details."
+        return 1
+    fi
+
+    echo "Frames have been saved in ${data_dir}."
 }
 
-# Function to cut a clip
+# Function to cut a specific portion from a video file
 cut_clip() {
-    read -p 'Enter start time (format: hh:mm:ss): ' start_time
-    read -p 'Enter end time (format: hh:mm:ss): ' end_time
-    ffmpeg -i "$INPUT_VIDEO" -ss "$start_time" -to "$end_time" -c copy "${OUTPUT_VIDEO}_cut.mp4"
-    echo "Cut operation completed. Output: ${OUTPUT_VIDEO}_cut.mp4"
+    local video=$1
+    local start_time=$2
+    local end_time=$3
+    local output_file="${4:-cut_clip.mp4}"
+
+    if [[ ! -f "$video" ]]; then
+        echo "Error: Video file not found."
+        return 1
+    fi
+
+    ffmpeg -i "$video" -ss "$start_time" -to "$end_time" -c copy "$output_file" 2>&1 | tee cut_clip.log
+
+    if [ "${PIPESTATUS[0]}" -ne 0 ]; then
+        echo "Error: Failed to cut clip. Check cut_clip.log for details."
+        return 1
+    fi
+
+    echo "Clip has been cut and saved as $output_file."
 }
 
-# Function to merge multiple clips
+# Function to merge multiple video clips into one file
 merge_clips() {
-    echo "Merging clips into $OUTPUT_VIDEO"
-    local input_txt="$(mktemp)"
-    local clip
-    local num_clips
-    read -p 'Enter number of clips to merge: ' num_clips
+    local output_file="${1:-merged_clip.mp4}"
+    local input_txt
+    input_txt="$(mktemp)"
+    shift
+    local input_files=("$@")
 
-    for (( i=1; i<=num_clips; i++ )); do
-        read -p "Enter name of clip $i: " clip
+    for clip in "${input_files[@]}"; do
         echo "file '$clip'" >> "$input_txt"
     done
 
-    ffmpeg -f concat -safe 0 -i "$input_txt" -c copy "${OUTPUT_VIDEO}_merged.mp4"
-    echo "Merge operation completed. Output: ${OUTPUT_VIDEO}_merged.mp4"
+    ffmpeg -f concat -safe 0 -i "$input_txt" -c copy "$output_file" 2>&1 | tee merge_clips.log
+
+    if [ "${PIPESTATUS[0]}" -ne 0 ]; then
+        echo "Error: Failed to merge clips. Check merge_clips.log for details."
+        return 1
+    fi
+
+    echo "Clips have been merged into $output_file."
     rm "$input_txt"
 }
 
-# Function to concatenate multiple videos
+# Function to concatenate multiple videos end-to-end
 concatenate_videos() {
-    echo "Concatenating videos into $OUTPUT_VIDEO"
-    local input_txt="$(mktemp)"
-    local video
-    local num_videos
-    read -p 'Enter number of videos to concatenate: ' num_videos
+    local output_file="${1:-concatenated_video.mp4}"
+    local input_txt
+    input_txt="$(mktemp)"
+    shift
+    local videos=("$@")
 
-    for (( i=1; i<=num_videos; i++ )); do
-        read -p "Enter name of video $i: " video
+    for video in "${videos[@]}"; do
         echo "file '$video'" >> "$input_txt"
     done
 
-    ffmpeg -f concat -safe 0 -i "$input_txt" -c copy "${OUTPUT_VIDEO}_concatenated.mp4"
-    echo "Concatenation operation completed. Output: ${OUTPUT_VIDEO}_concatenated.mp4"
+    ffmpeg -f concat -safe 0 -i "$input_txt" -c copy "$output_file" 2>&1 | tee concatenate_videos.log
+
+    if [ "${PIPESTATUS[0]}" -ne 0 ]; then
+        echo "Error: Failed to concatenate videos. Check concatenate_videos.log for details."
+        return 1
+    fi
+
+    echo "Videos have been concatenated into $output_file."
     rm "$input_txt"
 }
 
-# Main menu and operation execution
+# Main menu for user interaction
 main_menu() {
-    echo "Available Operations:"
-    echo "1) Capture Frames"
-    echo "2) Cut a Clip"
-    echo "3) Merge Clips"
-    echo "4) Concatenate Videos"
-    echo "5) Exit"
+    echo "=== // MENU // ==="
+    echo "1. Capture Frames"
+    echo "2. Cut a Clip"
+    echo "3. Merge Clips"
+    echo "4. Concatenate Videos"
+    echo "5. Exit"
+    read -r -p "Select an option: " option
 
-    read -p "Please select an operation by entering the corresponding number: " OPERATION
-
-    case "$OPERATION" in
-        1) capture_frames ;;
-        2) cut_clip ;;
-        3) merge_clips ;;
-        4) concatenate_videos ;;
-        5) exit 0 ;;
-        *) echo "Invalid option. Please try again." ;;
-    esac
+    case "$option" in
+        1)
+            read -r -p "Enter video file path: " video_file
+            read -r -p "Enter FPS (leave blank for default 30): " fps
+            capture_frames "$video_file" "$fps"
+            ;;
+        2)
+            read -r -p "Enter video file path: " video_file
+            read -r -p "Enter start time (format HH:MM:SS): " start_time
+            read -r -p "Enter end time (format HH:MM:SS): " end_time
+            read -r -p "Enter output file name (optional): " output_file
+            cut_clip "$video_file" "$start_time" "$end_time" "$output_file"
+            ;;
+        3)
+            read -r -p "Enter output file name (optional): " output_file
+            echo "Enter video clips to merge (end with an empty line):"
+            clips=()
+            while IFS= read -r clip; do
+            [[ $clip ]] || break
+            clips+=("$clip")
+            done
+            merge_clips "$output_file" "${clips[@]}"
+            ;;
+        4)
+            read -r -p "Enter output file name (optional): " output_file
+            echo "Enter videos to concatenate (end with an empty line):"
+            videos=()
+            while IFS= read -r video; do
+            [[ $video ]] || break
+            videos+=("$video")
+            done
+            concatenate_videos "$output_file" "${videos[@]}"
+            ;;
+        5)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Please try again."
+            main_menu
+            ;;
+            esac
 }
-
-# Execute the main menu function
 main_menu
-
-# Error handling for any unexpected situation
-trap 'error_exit "An unexpected error occurred. Exiting."' ERR
-
-exit 0
