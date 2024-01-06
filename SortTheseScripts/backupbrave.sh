e44703d370525d1f53d097b8e9d6c11cd1e89048
andro <97570105+4ndr0666@users.noreply.github.com>
2024-01-06 01:31:41 -0600

    updated force_vim_backupfile.sh

--- SortTheseScripts/backupbrave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define source and destination directories
-src_dir="$HOME/.config/BraveSoftware/Brave-Browser"
-dest_dir="/Nas/Nas/Brave_repo"
-
-# Create backup
-cp -r "$src_dir" "$dest_dir"
