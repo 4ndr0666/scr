a86a29ab92856d20f44363339c8505ad885b8cc4
andro <97570105+4ndr0666@users.noreply.github.com>
2024-01-12 16:18:52 -0600

    updated vacuum.py, all functional

--- clean/vacuum.py/vacuum.py
+++ clean/vacuum.py/vacuum.py
@@ -28,7 +28,6 @@ def prominent(message):
 def bug(message):
     print(f"{GREEN}{message}{NC}")
 
-
 # --- // Logging_setup:
 log_dir = os.path.join(os.path.expanduser("~"), ".local/share/system_maintenance")
 os.makedirs(log_dir, exist_ok=True)
@@ -76,8 +75,6 @@ def run_all_tasks(log_file):
         except Exception as e:
             log_and_print(format_message(f"Error executing task {key}: {e}", RED), 'error')
 
-
-
 #1 --- // Process_dependency_scan_log:
 def process_dep_scan_log(log_file):
     prominent(f"{INFO} Processing dependency scan log...")
@@ -105,22 +102,21 @@ def process_dep_scan_log(log_file):
         bug(f"{FAILURE} Dependency scan log file not found.")
     prominent(f"{SUCCESS} Dependency scan log processing completed.{SUCCESS}")
 
-
 #2 --- // Manage_Cron_Job_with_Corrected_Grep:
 def manage_cron_job(log_file):
     cron_command = f"find {log_dir} -name '*_permissions.log' -mtime +30 -exec rm {{}} \\;"
     cron_entry = f"0 0 * * * {cron_command}"
     try:
-    # --- // Deduplicate_cron_jobs_if_exist:
-        subprocess.run(["crontab", "-l"], check=True, stdout=subprocess.PIPE)
-        subprocess.run(["crontab", "-l", "|", "grep", "-v", "-F", cron_command], check=True, stdout=subprocess.PIPE)
-    # --- // Check_and_add_cron_job:
-        result = execute_command(["crontab", "-l", "|", "grep", "-F", cron_command], log_file)
+        # --- // Deduplicate_cron_jobs_if_exist:
+        subprocess.run(["sudo", "crontab", "-l"], check=True, stdout=subprocess.PIPE)
+        subprocess.run(["sudo", "crontab", "-l", "|", "grep", "-v", "-F", cron_command], check=True, stdout=subprocess.PIPE)
+        # --- // Check_and_add_cron_job:
+        result = execute_command(["sudo", "crontab", "-l", "|", "grep", "-F", cron_command], log_file)
         if result != 0:
             bug(f"{FAILURE} Cron job for deleting old logs not found. Setting up...")
             with open(os.path.expanduser("~/.cron_temp"), "w") as temp_file:
                 temp_file.write(f"{cron_entry}\n")
-                subprocess.run(["crontab", os.path.expanduser("~/.cron_temp")], check=True)
+                subprocess.run(["sudo", "crontab", os.path.expanduser("~/.cron_temp")], check=True)
                 shutil.rmtree(os.path.expanduser("~/.cron_temp"))
             prominent(f"{SUCCESS} Cron job set up to delete old logs.")
         else:
@@ -129,11 +125,11 @@ def manage_cron_job(log_file):
         bug(f"{FAILURE} Error: {e.stderr.strip()}")
 
 
-#3 --- // Remove_Broken_Symlinks_wSpinner:
+#3 --- // Remove_Broken_Symlinks:
 def remove_broken_symlinks(log_file):
     prominent(f"{INFO} Searching for broken symbolic links...")
     try:
-        links_found = subprocess.check_output(["find", "/", "-path", "/proc", "-prune", "-o", "-type", "l", "!", "-exec", "test", "-e", "{}", ";", "-print"], text=True)
+        links_found = subprocess.check_output(["sudo", "find", "/", "-path", "/proc", "-prune", "-o", "-type", "l", "!", "-exec", "test", "-e", "{}", ";", "-print"], text=True)
         links_found = links_found.strip()
         if not links_found:
             prominent(f"{INFO} No broken symbolic links found.")
@@ -142,7 +138,7 @@ def remove_broken_symlinks(log_file):
             choice = input("Do you wish to remove the above broken symbolic links? (y/n): ")
             if choice.lower() == "y":
                 try:
-                    subprocess.run(["find", links_found, "-delete"], check=True)
+                    subprocess.run(["sudo", "find", links_found, "-delete"], check=True)
                     prominent(f"{SUCCESS} Broken symbolic links removed.")
                 except subprocess.CalledProcessError as e:
                     bug(f"{FAILURE} Error: {e.stderr.strip()}")
@@ -165,7 +161,7 @@ def clean_old_kernels(log_file):
 #5 --- // Vacuum_Journalctl:
 def vacuum_journalctl(log_file):
     try:
-        subprocess.run(["journalctl", "--vacuum-time=3d"], check=True)
+        subprocess.run(["sudo", "journalctl", "--vacuum-time=3d"], check=True)
     except subprocess.CalledProcessError as e:
         bug(f"{FAILURE} Error: Failed to vacuum journalctl", log_file)
 
@@ -173,7 +169,7 @@ def vacuum_journalctl(log_file):
 #6 --- // Clear_Cache:
 def clear_cache(log_file):
     try:
-        subprocess.run(["find", "~/.cache/", "-type", "f", "-atime", "+3", "-delete"], check=True)
+        subprocess.run(["sudo", "find", "~/.cache/", "-type", "f", "-atime", "+3", "-delete"], check=True)
     except subprocess.CalledProcessError as e:
         bug(f"{FAILURE} Error: Failed to clear cache", log_file)
 
@@ -181,7 +177,7 @@ def clear_cache(log_file):
 #7 --- // Update_Font_Cache:
 def update_font_cache(log_file):
     try:
-        subprocess.run(["fc-cache", "-fv"], check=True)
+        subprocess.run(["sudo", "fc-cache", "-fv"], check=True)
     except subprocess.CalledProcessError as e:
         bug(f"{FAILURE} Error: Failed to update font cache", log_file)
 
@@ -191,7 +187,7 @@ def clear_trash(log_file):
     choice = input("Do you want to clear the trash? (y/n): ")
     if choice.lower() == "y":
         try:
-            subprocess.run(["rm", "-rf", "~/.local/share/Trash/*"], check=True)
+            subprocess.run(["sudo", "rm", "-rf", "~/.local/share/Trash/*"], check=True)
         except subprocess.CalledProcessError as e:
             bug(f"{FAILURE} Error: Failed to clear trash", log_file)
     else:
@@ -211,44 +207,44 @@ def optimize_databases(log_file):
 
 #10 --- // Clean_Package_Cache:
 def clean_package_cache(log_file):
-    prominent(f"{INFO} Cleaning package cache...")
+    prominent(f" {INFO}  Cleaning package cache...")
     try:
         subprocess.run(["sudo", "paccache", "-rk2"], check=True)
-        prominent(f"{SUCCESS} Package cache cleaned.")
+        prominent(f" {SUCCESS}  Package cache cleaned.")
     except subprocess.CalledProcessError as e:
-        bug(f"{FAILURE} Error: Failed to clean package cache", log_file)
+        bug(f" {FAILURE}  Error: Failed to clean package cache", log_file)
 
 
 #11 --- // Clean_AUR_dir:
 def clean_aur_directory(log_file):
     aur_dir = "/var/cache/pacman/pkg"  # Adjust this path according to your setup
     if os.path.isfile("/usr/local/bin/clean-aur-dir.py"):
-        prominent(f"{INFO} Cleaning AUR directory...")
+        prominent(f" {INFO}  Cleaning AUR directory...")
         try:
             subprocess.run(["python3", "/usr/local/bin/clean-aur-dir.py", aur_dir], check=True)
-            prominent(f"{SUCCESS} AUR directory cleaned.")
+            prominent(f" {SUCCESS}  AUR directory cleaned.")
         except subprocess.CalledProcessError as e:
-            bug(f"{FAILURE} Error: Failed to clean AUR directory", log_file)
+            bug(f" {FAILURE}  Error: Failed to clean AUR directory", log_file)
     else:
-        bug(f"{FAILURE} clean-aur-dir.py script not found.", log_file)
+        bug(f" {FAILURE}  clean-aur-dir.py script not found.", log_file)
 
 
 #12 --- // Handle_Pacnew_and_Pacsave_Files:
 def handle_pacnew_pacsave(log_file):
-    pacnew_files = subprocess.check_output(["find", "/etc", "-type", "f", "-name", "*.pacnew"], text=True).splitlines()
-    pacsave_files = subprocess.check_output(["find", "/etc", "-type", "f", "-name", "*.pacsave"], text=True).splitlines()
+    pacnew_files = subprocess.check_output(["sudo", "find", "/etc", "-type", "f", "-name", "*.pacnew"], text=True).splitlines()
+    pacsave_files = subprocess.check_output(["sudo", "find", "/etc", "-type", "f", "-name", "*.pacsave"], text=True).splitlines()
     if pacnew_files:
-        prominent(f"{INFO} .pacnew files found. Consider merging:")
+        prominent(f" {INFO}  .pacnew files found. Consider merging:")
         for file in pacnew_files:
             print(file)
     else:
-        prominent(f"{INFO} No .pacnew files found.")
+        prominent(f" {INFO}  No .pacnew files found.")
     if pacsave_files:
-        prominent(f"{INFO} .pacsave files found. Consider reviewing:")
+        prominent(f" {INFO}  .pacsave files found. Consider reviewing:")
         for file in pacsave_files:
             print(file)
     else:
-        prominent(f"{INFO} No .pacsave files found.")
+        prominent(f" {INFO}  No .pacsave files found.")
 
 
 #13 --- // Verify_Installed_Packages:
@@ -256,38 +252,39 @@ def verify_installed_packages(log_file):
     prominent(f"{INFO} Verifying installed packages...")
     try:
         subprocess.run(["sudo", "pacman", "-Qkk"], check=True)
-        prominent(f"{SUCCESS} All installed packages verified.")
+        prominent(f" {SUCCESS}  All installed packages verified.")
     except subprocess.CalledProcessError as e:
-        bug(f"{FAILURE} Error: Issues found with installed packages", log_file)
+        bug(f" {FAILURE}  Error: Issues found with installed packages", log_file)
 
 
 #14 --- // Check_failed_cron_jobs:
 def check_failed_cron_jobs(log_file):
-    prominent(f"{INFO} Checking for failed cron jobs...")
+    prominent(f" {INFO}  Checking for failed cron jobs...")
     try:
         subprocess.run(["grep", "-i", "cron.*error", "/var/log/syslog"], check=True)
-        prominent(f"{FAILURE} Failed cron jobs detected. Review syslog for details.")
+        prominent(f" {FAILURE}  Failed cron jobs detected. Review syslog for details.")
     except subprocess.CalledProcessError as e:
-        prominent(f"{INFO} No failed cron jobs detected.", log_file)
+        prominent(f" {INFO}  No failed cron jobs detected.", log_file)
 
 
 #15 --- // Clear_Docker_Images:
 def clear_docker_images(log_file):
     if shutil.which("docker"):
         try:
+            subprocess.run(["sudo", "docker", "image", "prune", "-f"], check=True)
             subprocess.run(["docker", "image", "prune", "-f"], check=True)
         except subprocess.CalledProcessError as e:
-            bug(f"{FAILURE} Error: Failed to clear Docker images", log_file)
+            bug(f" {FAILURE}  Error: Failed to clear Docker images", log_file)
     else:
-        prominent(f"{INFO} Docker is not installed. Skipping Docker image cleanup.", log_file)
+        print(f" {INFO}  Docker is not installed. Skipping Docker image cleanup.", log_file)
 
 
 #16 --- // Clear_temp_folder:
 def clear_temp_folder(log_file):
     try:
-        subprocess.run(["find", "/tmp", "-type", "f", "-atime", "+2", "-delete"], check=True)
+        subprocess.run(["sudo", "find", "/tmp", "-type", "f", "-atime", "+2", "-delete"], check=True)
     except subprocess.CalledProcessError as e:
-        bug(f"{FAILURE} Error: Failed to clear the temporary folder", log_file)
+        bug(f" {FAILURE}  Error: Failed to clear the temporary folder", log_file)
 
 
 #17 --- Run_rmshit.py:
@@ -296,9 +293,9 @@ def check_rmshit_script(log_file):
         try:
             subprocess.run(["python3", "/usr/local/bin/rmshit.py"], check=True)
         except subprocess.CalledProcessError as e:
-            bug(f"{FAILURE} Error: Failed to run rmshit.py", log_file)
+            bug(f" {FAILURE}  Error: Failed to run rmshit.py", log_file)
     else:
-        prominent(f"{INFO} python3 or rmshit.py not found. Skipping.", log_file)
+        prominent(f" {INFO}  python3 or rmshit.py not found. Skipping.", log_file)
 
 
 #18 --- // Remove_old_SSH_known_hosts:
@@ -306,11 +303,12 @@ def remove_old_ssh_known_hosts(log_file):
     ssh_known_hosts_file = os.path.expanduser("~/.ssh/known_hosts")
     if os.path.isfile(ssh_known_hosts_file):
         try:
+            subprocess.run(["sudo", "find", ssh_known_hosts_file, "-mtime", "+14", "-exec", "sed", "-i", "{}d", "{}", ";"], check=True)
             subprocess.run(["find", ssh_known_hosts_file, "-mtime", "+14", "-exec", "sed", "-i", "{}d", "{}", ";"], check=True)
         except subprocess.CalledProcessError as e:
-            bug(f"{FAILURE} Error: Failed to remove old SSH known hosts entries", log_file)
+            bug(f" {FAILURE}  Error: Failed to remove old SSH known hosts entries", log_file)
     else:
-        prominent(f"{INFO} No SSH known hosts file found. Skipping.", log_file)
+        prominent(f" {INFO}  No SSH known hosts file found. Skipping.", log_file)
 
 
 #19 --- Remove_orphan_Vim_undo_files:
@@ -324,31 +322,33 @@ def remove_orphan_vim_undo_files(log_file):
                         os.remove(file)
                     except OSError as e:
                         bug(f" {FAILURE}  Error: Failed to remove orphan Vim undo files: {e}", log_file)
-    prominent(f" {SUCCESS}  Orphan Vim undo files removed.", log_file)
+    print(f" {SUCCESS}  Orphan Vim undo files removed.", log_file)
 
 
 #20 --- // Force_log_rotation:
 def force_log_rotation(log_file):
     try:
-        subprocess.run(["logrotate", "-f", "/etc/logrotate.conf"], check=True)
+        subprocess.run(["sudo", "logrotate", "-f", "/etc/logrotate.conf"], check=True)
     except subprocess.CalledProcessError as e:
         bug(f" {FAILURE}  Error: Failed to force log rotation", log_file)
 
 
 #21 --- // Configure_ZRam:
 def configure_zram(log_file):
-    prominent(f" {INFO}  Configuring ZRam for better memory management...")
+    prominent(f"{INFO} Configuring ZRam for better memory management...")
     if shutil.which("zramctl"):
         try:
-            mem_total = int(subprocess.check_output(["awk", "/MemTotal/{printf \"%d\n\", $2 * 1024 * 0.25}", "/proc/meminfo"]).decode().strip())
-            subprocess.run(["sudo", "zramctl", "--find", f"--size={mem_total}"], check=True)
-            subprocess.run(["sudo", "mkswap", "/dev/zram0"], check=True)
-            subprocess.run(["sudo", "swapon", "/dev/zram0", "-p", "32767"], check=True)
-            prominent(f"{SUCCESS} ZRam configured successfully.")
+            mem_total_cmd = ["awk", '/MemTotal/{printf "%d\\n", $2 * 1024 * 0.25}', "/proc/meminfo"]
+            mem_total = int(subprocess.check_output(mem_total_cmd).decode().strip())
+            zram_device = subprocess.run(["sudo", "zramctl", "--find", f"--size={mem_total}"], stdout=subprocess.PIPE, text=True, check=True).stdout.strip()
+            subprocess.run(["sudo", "mkswap", zram_device], check=True)
+            subprocess.run(["sudo", "swapon", zram_device, "-p", "32767"], check=True)
+            prominent(f"{SUCCESS} ZRam configured successfully on {zram_device}.")
         except subprocess.CalledProcessError as e:
-            bug(f" {FAILURE}  Error: {e.stderr.strip()}", log_file)
+            bug(f"{FAILURE} Error: {e.stderr.decode().strip()}" if e.stderr else "Error occurred, but no stderr output.")
     else:
-        bug(f" {FAILURE}  ZRam not available. Consider installing it first.", log_file)
+        bug(f"{FAILURE} ZRam not available. Consider installing it first.")
+
 
 
 #22 --- // Check_ZRam_configuration:
@@ -362,23 +362,23 @@ def check_zram_configuration(log_file):
 #23 --- // Adjust_swappiness:
 def adjust_swappiness(log_file):
     swappiness_value = 10  # Recommended for systems with low RAM
-    prominent(f"{INFO} Adjusting swappiness to {swappiness_value}...")
+    prominent(f" {INFO}  Adjusting swappiness to {swappiness_value}...")
     try:
         subprocess.run(["echo", str(swappiness_value), "|", "sudo", "tee", "/proc/sys/vm/swappiness"], check=True)
         subprocess.run(["sudo", "sysctl", f"vm.swappiness={swappiness_value}"], check=True)
-        prominent(f"{SUCCESS} Swappiness adjusted to {swappiness_value}.")
+        prominent(f" {SUCCESS}  Swappiness adjusted to {swappiness_value}.")
     except subprocess.CalledProcessError as e:
-        bug(f"{FAILURE} Error: Failed to adjust swappiness", log_file)
+        bug(f" {FAILURE}  Error: Failed to adjust swappiness", log_file)
 
 
 #24 --- // Clear_system_cache:
 def clear_system_cache(log_file):
-    prominent(f"{INFO} Clearing PageCache, dentries, and inodes...")
+    prominent(f" {INFO}  Clearing PageCache, dentries, and inodes...")
     try:
         subprocess.run(["echo", "1", "|", "sudo", "tee", "/proc/sys/vm/drop_caches"], check=True)
-        prominent(f"{SUCCESS} System caches cleared.")
+        prominent(f" {SUCCESS}  System caches cleared.")
     except subprocess.CalledProcessError as e:
-        bug(f"{FAILURE} Error: Failed to clear system cache", log_file)
+        bug(f" {FAILURE}  Error: Failed to clear system cache", log_file)
 
 
 #25 --- // Disable_unused_services:
@@ -388,25 +388,25 @@ def disable_unused_services(log_file):
         try:
             if subprocess.run(["systemctl", "is-enabled", service], stdout=subprocess.PIPE).returncode == 0:
                 subprocess.run(["sudo", "systemctl", "disable", service], check=True)
-                prominent(f"{SUCCESS} Disabled {service}.", log_file)
+                print(f" {SUCCESS}  Disabled {service}.", log_file)
             else:
-                prominent(f"{INFO} {service} is already disabled.", log_file)
+                print(f" {INFO}  {service} is already disabled.", log_file)
         except subprocess.CalledProcessError as e:
-            bug(f"{FAILURE}Error:Failed to disable{service}",log_file)
+            bug(f" {FAILURE}  Error:Failed to disable{service}",log_file)
 
 
 #26 --- // Check_failed_systemd_units:
 def check_and_restart_systemd_units(log_file):
-        prominent(f"{INFO} Checking and restarting systemd units...")
+        prominent(f" {INFO}  Checking and restarting systemd units...")
         try:
             # Use 'sysz' to check for failed units and restart them
             result = execute_command(["sysz"], log_file)
             if result == 0:
-                prominent(f"{SUCCESS} Systemd units checked and restarted successfully.")
+                prominent(f" {SUCCESS}  Systemd units checked and restarted successfully.")
             else:
-                bug(f"{FAILURE} Error checking and restarting systemd units.")
+                bug(f" {FAILURE}  Error checking and restarting systemd units.")
         except subprocess.CalledProcessError as e:
-            bug(f"{FAILURE} Error checking and restarting systemd units: {e.stderr.strip()}")
+            bug(f" {FAILURE}  Error checking and restarting systemd units: {e.stderr.strip()}")
 
 # --- // Define_menuoptions_with_partial:
 menu_options = {
@@ -483,4 +483,4 @@ if __name__ == "__main__":
     main()
 
 
-log.file.close()
+log_file.close()
