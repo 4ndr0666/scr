403a6fda977567b122dfc1abcbc32033c2a376f8
andro <97570105+4ndr0666@users.noreply.github.com>
2024-03-01 15:08:40 -0600

    updating changes from local repo after migrating

---
+++ clean/merge_hardcode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Merge contents of TheCLoudold and TheCLoudold1 into C
+rsync -av --ignore-existing --ignore-times --update --progress --recursive /23.1/TheCLoudold/ /23.1/C
+rsync -av --ignore-existing --ignore-times --update --progress --recursive /23.1/TheCLoudold1/ /23.1/C
+
+# Verify merge success
+if [ $? -eq 0 ]; then
+    echo "Merge successful. Deleting source directories."
+
+    # Delete TheCLoudold and TheCLoudold1 if merge was successful
+    rm -r /23.1/TheCLoudold
+    rm -r /23.1/TheCLoudold1
+
+    echo "Source directories deleted."
+else
+    echo "Merge encountered errors. Source directories were not deleted."
+fi
