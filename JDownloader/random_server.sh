ea7d484d4b4cc85f6466a053d3c4f04c3c8cb3f0
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2024-05-24 16:15:34 -0500

    added jdownloader script

---
+++ JDownloader/random_server.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+VPN_SERVERS=(
+  "smart"
+  "hk2"
+  "hk1"
+  "usda"
+  "usny"
+  "usla2"
+  "uswd"
+  "usse"
+  "usph"
+  "usnj1"
+  "usla1"
+  "ussf"
+  "usmi"
+  "usmi2"
+  "usla3"
+  "usnj3"
+  "usat"
+  "usde"
+  "uslp"
+  "usho"
+  "usal"
+  "sgju"
+  "sgcb"
+  "inuk"
+  "insi"
+  "ausy"
+  "aume"
+  "auwo"
+  "aubr"
+  "aupe"
+  "auad"
+  "cato2"
+  "cato"
+  "camo"
+  "defr3"
+  "defr1"
+  "denu"
+  "mx"
+  "ukdo"
+  "uklo"
+  "ukel"
+  "ukmi"
+  "nlam"
+  "nlro"
+  "nlth"
+  "esma"
+  "esba2"
+  "ie"
+  "frpa2"
+  "frma"
+  "itco"
+)
+
+random_server() {
+  SERVER=${VPN_SERVERS[$RANDOM % ${#VPN_SERVERS[@]}]}
+  expressvpn connect $SERVER
+}
+
+while true; do
+  random_server
+done
+
