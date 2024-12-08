e2341c9d881d9b3218e5867ec34dd47ddeedf88e
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2024-12-07 23:25:04 -0600

    Auto-commit: 0 added, 1 modified, 0 deleted

--- 4ndr0tools/4ndr0service/settings.sh
+++ 4ndr0tools/4ndr0service/settings.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Author: 4ndr0666
-################################
-##############################
+### Author: 4ndr0666
+##
+######################################################
+####################################### // CONSTANTS //
 export LOG_FILE_DIR="/home/andro/.local/logs/4ndr0service/logs"
 export LOG_FILE="$LOG_FILE_DIR/service_optimization.log"
 export BACKUP_DIR="/Nas/Backups/4ndr0service"
@@ -11,12 +12,12 @@ export SETTINGS_FILE="$PKG_PATH/settings.sh"
 ######### --- // DIALOG or CLI
 export USER_INTERFACE="dialog"
 ###############################
-############################## // TEXT EDITOR //
-#############################
+########################### // TEXT EDITOR //
+##########################
 export SETTINGS_EDITOR="nvim"
-###############################
-#############################
 ############################
+###########################
+##########################
 create_directory_if_not_exists "$LOG_FILE_DIR"
 create_directory_if_not_exists "$BACKUP_DIR"
 #######################################################
